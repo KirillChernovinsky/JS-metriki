89539bed5cf634382362e35b80cc69762d169793
Kirill_Chernovinskii <151826585+KirillChernovinsky@users.noreply.github.com>
2024-02-15 00:32:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -193,4 +193,273 @@
 //     document.body.appendChild(clonedInput);
 // }
 
+// Потомки
 
+// 1
+// window.onload = function() {
+//     let elem = document.getElementById('elem');
+//     elem.firstElementChild.classList.add('red');
+// };
+
+// 2
+// window.onload = function() {
+//     let elem = document.getElementById('elem');
+//     elem.lastElementChild.classList.add('red');
+// };
+
+// 3
+
+// window.onload = function() {
+//     let elem = document.getElementById('elem');
+//     let descendants = elem.getElementsByTagName("*");
+
+// for (let i = 0; i < descendants.length; i++) {
+//   descendants[i].innerHTML += "!";
+// }
+// };
+
+// Соседи
+// 1
+
+// let elem = document.getElementById('elem');
+// let prevSibling = elem.previousElementSibling;
+// prevSibling.innerHTML = prevSibling.innerHTML + '!';
+
+
+// 2
+
+// let elem = document.getElementById('elem');
+// let prevSibling = elem.nextElementSibling;
+// prevSibling.innerHTML = prevSibling.innerHTML + '!';
+
+
+// 3
+
+// let elem = document.getElementById('elem');
+// let prevSibling = elem.nextElementSibling;
+// let prevprevSilbling = prevSibling.nextElementSibling
+// prevprevSilbling.innerHTML = prevprevSilbling.innerHTML + '!';
+
+
+
+// родители
+
+// 1
+// let elem = document.getElementById('elem');
+// let parent = elem.parentNode;
+// parent.style.color = 'red';
+
+// 2
+
+// let elem = document.getElementById('elem');
+// let parent = elem.parentNode;
+// let parentparent = parent.parentNode;
+// parentparent.style.color = 'red';
+
+// На величину границы
+// 1
+
+// function borde() {
+//     let elem = document.getElementById("elem");
+//     let style = getComputedStyle(elem);
+//     let borderTopWidth = style.getPropertyValue("border-top-width");
+//     alert("Толщина" + borderTopWidth);
+//   }
+
+
+//2
+// function borde() {
+//     let elem = document.getElementById("elem");
+//     let style = getComputedStyle(elem);
+//     let borderLeftWidth = style.getPropertyValue("border-left-width");
+//     alert("Толщина" + borderLeftWidth);
+//   }
+
+
+// на полный размер элемента
+
+//1
+// function calculateThickness() {
+//     let elem = document.getElementById("elem");
+//     let totalWidth = elem.offsetWidth;
+//     console.log("Total Width: " + totalWidth + "px");
+
+// }
+
+
+// 2
+
+// function calculateThickness() {
+//     let elem = document.getElementById("elem");
+//     let totalHeight = elem.offsetHeight;
+//     console.log("Total Height: " + totalHeight + "px");
+// }
+
+// На размер эелемента без границ, но с padding
+
+// 1 
+
+// function calculateThickness() {
+//     let elem = document.getElementById('elem');
+//     let width = elem.clientWidth;
+    
+//     let style = getComputedStyle(elem);
+//     let paddingX = parseInt(style.paddingLeft) + parseInt(style.paddingRight);
+    
+//     let thicknessX = width + paddingX;
+//     console.log('height: ' + thicknessY);
+//   }
+
+
+// 2
+
+// function calculateThickness() {
+//     let elem = document.getElementById('elem');
+//     let height = elem.clientHeight;
+    
+//     let style = getComputedStyle(elem);
+//     let paddingY = parseInt(style.paddingTop) + parseInt(style.paddingBottom);
+    
+//     let thicknessY = height + paddingY;
+    
+//     console.log('height: ' + thicknessY);
+//   }
+
+
+
+// Работа с getComputedStyle
+
+// 1
+// let elem = document.getElementById('elem');
+// let computedStyle = getComputedStyle(elem);
+
+// let width = parseInt(computedStyle.width, 10); // для указания системы счистления
+// let height = parseInt(computedStyle.height, 10);
+
+// console.log('Ширина:', width);
+// console.log('Высота:', height);
+
+
+// Прокрутка эелемента
+
+//1
+
+// let button = document.getElementById('scrollButton');
+// let elem = document.getElementById('elem');
+
+// button.addEventListener('click', function() {
+//   console.log('scrollHeight:', elem.scrollHeight);
+// });
+
+// 2
+
+// let button = document.getElementById('scrollButton');
+// let elem = document.getElementById('elem');
+
+// button.addEventListener('click', function() {
+//   console.log('scrollTop:', elem.scrollLeft);
+// });
+
+//3 
+// let button = document.getElementById('scrollButton');
+// let elem = document.getElementById('elem');
+
+// button.addEventListener('click', function() {
+//   console.log('scrollTop:', elem.scroll);
+// });
+
+// 4
+
+// document.getElementById('scrollButton').addEventListener('click', function() {
+//     let elem = document.getElementById('elem');
+//     elem.scrollTop = 100;
+// });
+
+// 5
+
+// document.getElementById('scrollButton').addEventListener('click', function() {
+//     let elem = document.getElementById('elem');
+//     elem.scrollHeight = 100;
+// });
+
+
+// Прокрутка страницы
+
+// 1
+
+// document.getElementById('scrollButton').addEventListener('click', function() {
+//     let scrollAmount = window.pageYOffset;
+//     alert('Прокручено: ' + scrollAmount + ' px');
+//   });
+
+// 2
+
+// document.getElementById('scrollButton').addEventListener('click', function() {
+//     let scrollAmount = window.pageXOffset;
+//     alert('Прокручено: ' + scrollAmount + ' px');
+//   });
+
+
+// Прокрутка страницы
+
+//1
+// function pos() {
+//     window.scrollTo({
+//       top: 500,
+//       left: 300,
+//       behavior: 'smooth' // Для плавной прокрутки
+//     });
+//   }
+//2
+
+// function pos() {
+//     window.scrollTo({
+//       down: 300,
+//       behavior: 'smooth' // Для плавной прокрутки
+//     });
+//   }
+
+
+
+// Практика 
+
+// 1
+document.getElementById("button").addEventListener("click", function(){
+    window.scrollTo(0,document.body.scrollHeight);
+  });
+
+
+// 2
+function scrollPageUp() {
+    window.scrollBy(0, -400);
+  }
+
+
+// 3
+
+// window.onscroll = function() {
+//     if (isPageScrolledToBottom()) {
+//         scrollPageUp();
+//     }
+// };
+
+// function scrollPageUp() {
+//     window.scrollBy(0, -100);
+// }
+
+// function isPageScrolledToBottom() {
+//     return window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+// }
+
+
+// 4 
+
+// let element = document.querySelector("#elem");
+
+// element.addEventListener("click", function() {
+//   let Width = element.offsetWidth;
+//   let Height = element.offsetHeight;
+
+//   element.style.width = (Width * 2) + "px";
+//   element.style.height = (Height * 2) + "px";
+// });
